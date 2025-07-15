133ad2f03b68c46afdbaded2a9dd101957abf762
Jaden Meharry <jaden.meharry@kingsway.school.nz>
2025-07-15 12:08:43 +1200

    fixed player speed depending on fps
    I fixed the player speed to stay the same no matter of the fps set by the variable called vel

--- game/tests and examples/main.py
+++ game/tests and examples/main.py
@@ -1,30 +1,42 @@
 #platformer demo
 #r4t0030
-#version 1.1
+#version 1.2
 #2025-06-24
-# TODO: make the main window and sprite to move
+# TODO: optimise an make a jump buttion
 import pygame
 pygame.init()
 
 #display
-win = pygame.display.set_mode((500, 500))
+screen = ((500, 500))
+win = pygame.display.set_mode((screen))
+
+#useing this to set border collisions
+screen_width = 500
+
 #title
 pygame.display.set_caption("Platformer Demo")
 
-# set the co-ordinates of where the sprite will appear and its hight/width
-x = 50
-y = 50
-#width and hight
+#frame rate
+clock = pygame.time.Clock()
+#width and hight for the sprite
 width = 50
 height = 50
-
 # set velocity to control the speed of the sprite
-vel = 1.5
+vel = 200
+
+# set the co-ordinates of where the sprite will appear and its hight/width
+x = screen[0]/2 - width/2
+y = screen[1]/2 - height/2
+
+#jump code
+isJump = False
+jumpHight = 10
 
 #game loop
 done = True
 while done:
-    pygame.time.delay(5) #controls the frame rate
+    dt = clock.tick(60) / 1000.0 # Limit to 60 frames per second and get delta time in seconds # Limit to 60 frames per second #controls the frame rate
+    print("FPS:", int(clock.get_fps()))
 
     #quit the game when the window gets closed
     for event in pygame.event.get():
@@ -37,13 +49,13 @@ while done:
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_LEFT]:
-        x -= vel
+        x -= vel * dt
     if keys[pygame.K_RIGHT]:
-        x += vel
+        x += vel * dt
     if keys[pygame.K_UP]:
-        y -= vel
+        y -= vel * dt
     if keys[pygame.K_DOWN]:
-        y += vel
+        y += vel * dt
     if keys[pygame.K_ESCAPE]:
         done = False
     #draw over the rectangle to cover up its tracks
